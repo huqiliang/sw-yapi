37bf6e49741efd1c3fbd71bfbaa42efe04d52218
huqiliang <181831335@qq.com>
2018-08-17 10:59:26 +0800

    update

--- app.js
+++ app.js
@@ -20,9 +20,11 @@ app.use(
 );
 app.use(json());
 app.use(logger());
-app.use(require("koa-static-plus")(__dirname + "/public"), {
-  pathPrefix: "/swagger"
-});
+app.use(
+  require("koa-static-plus")(__dirname + "/public", {
+    pathPrefix: "/swagger"
+  })
+);
 
 app.use(
   views(__dirname + "/views", {
